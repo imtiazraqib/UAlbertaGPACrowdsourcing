7db989ee9fe383fe01732d7785a7485cb6aad38f
imtiazraqib <imtiazud@ualberta.ca>
2019-08-12 12:43:10 -0600

    ADD: Tabletop.js ; UPDATE: scraping google sheet NOT COMPLETE

--- webpage/js/main.js
+++ webpage/js/main.js
@@ -1,2 +1,28 @@
 window.onload = function() {
+    init()
+};
+
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/11FzPr71WSyy6JclrSpzUxV0IbuntnMRuPsFjWzlAZPY/pubhtml';
+
+function init() {
+    Tabletop.init({
+        key: public_spreadsheet_url,
+        callback: function(data, tabletop) {
+            console.log(data)
+        },
+        simpleSheet: true
+    })
+}
+window.addEventListener('DOMContentLoaded', init)
+
+function showInfo(data, tabletop) {
+    var tr;
+    for (var i = 0; i < data.length; i++) {
+        tr = $('<tr/>');
+        tr.append("<td scope='col' class='text-center faculty'>" + data[i].Faculty + "</td>");
+        tr.append("<td scope='col' class='text-center dept'>" + data[i].Department + "</td>");
+
+        $('table').append(tr);
+    }
+    console.log(data);
 }
