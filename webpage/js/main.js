7b084f1d5c0c10bec108303fdfd9dc50230dbb09
imtiazraqib <imtiazud@ualberta.ca>
2019-08-20 13:38:35 -0600

    UPDATE: can retrieve data and display from sheets; ERROR: DataTables display

--- webpage/js/main.js
+++ webpage/js/main.js
@@ -2,27 +2,29 @@ window.onload = function() {
     init()
 };
 
-var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/11FzPr71WSyy6JclrSpzUxV0IbuntnMRuPsFjWzlAZPY/pubhtml';
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1L7L3FaIk39x4cSTEQvE1XIgS48L3AP1IupJUZQXXKwM/edit?usp=sharing';
 
 function init() {
     Tabletop.init({
         key: public_spreadsheet_url,
-        callback: function(data, tabletop) {
-            console.log(data)
-        },
+        callback: showInfo,
         simpleSheet: true
     })
 }
-window.addEventListener('DOMContentLoaded', init)
 
 function showInfo(data, tabletop) {
     var tr;
     for (var i = 0; i < data.length; i++) {
-        tr = $('<tr/>');
-        tr.append("<td scope='col' class='text-center faculty'>" + data[i].Faculty + "</td>");
-        tr.append("<td scope='col' class='text-center dept'>" + data[i].Department + "</td>");
-
+        tr = $("<tr role='row' class='odd'>");
+        tr.append("<td scope='col' class='text-center faculty'>" + data[i].Department + "</td>");
+        tr.append("<td scope='col' class='text-center dept'>" + data[i].CourseNumber + "</td>");
+        tr.append("<td scope='col' class='text-center dept'>" + data[i].CourseGPAAverage + "</td>");
+        tr.append("<td scope='col' class='text-center dept'>" + data[i].Term + "</td>");
+        tr.append("<td scope='col' class='text-center dept'>" + data[i].Professor + "</td>");
+        tr.append("<td scope='col' class='text-center dept'>" + data[i].ClassSize + "</td>");
+        tr.append("<td scope='col' class='text-center dept'>" + data[i].Faculty + "</td>");
+        tr.append("</tr>")
         $('table').append(tr);
     }
-    console.log(data);
 }
+window.addEventListener('DOMContentLoaded', init)
